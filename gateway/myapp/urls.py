7b1a6774ae80763aeabf647a2a33639a1cd5282d
SatriaAPN <satrianusa10@gmail.com>
2024-12-24 01:10:44 +0800

    feat: implement new api for blog creation

--- gateway/myapp/urls.py
+++ gateway/myapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import auth_login_view, auth_register_view, test_view  # Import the views
+from .views import auth_login_view, auth_register_view, create_blog_view, test_view  # Import the views
 from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     path('auth/login', auth_login_view, name='login'),
     path('auth/register', auth_register_view, name='login'),
+    path('blogs', create_blog_view, name='createBlog'),
     path('test', test_view, name='test'),
 ]
